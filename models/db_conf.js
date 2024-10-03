62aea73411ab029f454482ef6a963273d8992c55
Abdellah Azzouz <abdellah.azzouz@student.vinci.be>
2024-10-03 17:15:29 +0200

    Change header and footer colors

--- models/db_conf.js
+++ models/db_conf.js
@@ -1,6 +1,4 @@
-// file db_conf.js in models
-
-const db = require('better-sqlite3')('/opt/web1_23-24/db/exoplanets.db', { verbose: console.log });
-// const db = require('better-sqlite3')('/home/olivier/exoplanets.db', { verbose: console.log });
+const path = require('path');
+const db = require('better-sqlite3')(path.join(__dirname, '..', 'db', 'exoplanets.db'), { verbose: console.log });
 
 module.exports = db;
